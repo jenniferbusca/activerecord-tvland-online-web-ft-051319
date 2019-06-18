9e67ba12afd43b794dcd3af6d0bb25cec8eb4be7
jenniferbusca <jennifer.busca@gmail.com>
2019-06-18 16:51:02 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,10 +1,11 @@
 class Show < ActiveRecord::Base
   has_many :characters
+  has_many   :actors, through: :characters
   belongs_to :network
-
-  def build_network(attributes)
-    network = Network.create(attributes)
-    self.network = network
-  end
+  
+  # def build_network(attributes)
+  #   network = Network.create(attributes)
+  #   self.network = network
+  # end
 
 end
